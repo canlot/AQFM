05399ba83a921255277c3dbb7f2ee8a92dc455e9
canlot <jakobsasmuzki@gmx.de>
2014-09-17 15:15:42 +0200

    Add Connector Klasses

---
+++ lib_Conn_manager/DbCon.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: Jake
+ * Datum: 17.09.2014
+ * Zeit: 14:28
+ * 
+ * Sie können diese Vorlage unter Extras > Optionen > Codeerstellung > Standardheader ändern.
+ */
+using System;
+using MySql.Data.MySqlClient;
+
+namespace lib_Conn_manager
+{
+	/// <summary>
+	/// Description of DbCon.
+	/// </summary>
+	public class DbCon
+	{
+		private bool logged = false;
+		
+		public bool is_logged
+		{
+			get
+			{
+				return logged;
+			}
+			private set
+			{
+				logged = value;
+			}
+		}
+		
+		
+		MySql.Data.MySqlClient.MySqlConnection conn;
+		private string DbName;
+		private string DbHost;
+		private string UserName;
+		private string UserPassword;
+		
+		public DbCon(string name, string host, string user, string password)
+		{
+			DbName = name;
+			DbHost = host;
+			UserName = user;
+			UserPassword = password;
+			
+		}
+	}
+}
