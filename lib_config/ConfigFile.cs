fa99464b3b7170d13ddb657f30f2bdef1b821c8c
canlot <jakobsasmuzki@gmx.de>
2014-09-17 23:38:44 +0200

    ConfigFile added for Connection stuff

---
+++ lib_config/ConfigFile.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: Jake
+ * Datum: 17.09.2014
+ * Zeit: 23:13
+ * 
+ * Sie können diese Vorlage unter Extras > Optionen > Codeerstellung > Standardheader ändern.
+ */
+using System;
+using System.Data;
+using System.IO;
+
+namespace lib_config
+{
+	/// <summary>
+	/// Description of ConfigFile.
+	/// </summary>
+	public class ConfigFile
+	{
+		//FileInfo file;
+		public ConfigFile(string name)
+		{
+			
+			//file  = new FileInfo(name);
+			
+		}
+	}
+}
