910e33b080ed719dccf29f5e258390c9bc87099a
canlot <jakobsasmuzki@gmx.de>
2014-10-04 10:59:03 +0200

    Added DbOutgoings

---
+++ lib_Conn_manager/DbOutgoings.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Erstellt mit SharpDevelop.
+ * Benutzer: Jakob
+ * Datum: 03.10.2014
+ * Zeit: 15:04
+ * 
+ * Sie können diese Vorlage unter Extras > Optionen > Codeerstellung > Standardheader ändern.
+ */
+using System;
+
+namespace lib_Conn_manager
+{
+	/// <summary>
+	/// Description of DbOutgoings.
+	/// </summary>
+	public class DbOutgoings
+	{
+		public DbOutgoings()
+		{
+		}
+	}
+}
